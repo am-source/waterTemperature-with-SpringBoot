8bb9e9c01de6d849949ceb6f6e7e3b8ff9d1bc65
am-source <a.mardini@gmx.net>
2023-02-09 08:38:29 +0100

    setup project

---
+++ src/main/java/com/amsource/waterTemperatureDemo/WaterTemperatureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.amsource.waterTemperatureDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterTemperatureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterTemperatureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amsource/waterTemperatureDemo/WaterTemperatureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsource.waterTemperatureDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterTemperatureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
